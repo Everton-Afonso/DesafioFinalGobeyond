a1e28f1507eebceb6483d8aa7108b4c9d0775d6b
Everton Otavio Afonso <evertonafonso0@gmail.com>
2021-09-24 14:31:35 -0300

    melhorias de performance da footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -81,29 +81,29 @@ function Cards() {
         </section>
 
         <section className="footer-countries">
-          <div>
+          <section className="countries">
             <h3>.Brasil</h3>
             <p>Avenida Andrômeda, 2000. Bloco 6 e 8 Alphaville SP</p>
             <p>R. Ifigenia Maria de Oliveira, 3793 Jr. Piratininga Franca SP</p>
-          </div>
+          </section>
 
-          <div>
+          <section className="countries">
             <h3>.Argentina</h3>
             <p>Soler 5518, 3 Piso C1425BYF Palermo Hollywood CABA</p>
-          </div>
+          </section>
 
-          <div>
+          <section className="countries">
             <h3>.México</h3>
             <p>
               Blvd. Miguel de Cervantes Saavedra 169, Granada, Miguel Hidalgo,
               11520 Ciudad de México, CDMX
             </p>
-          </div>
+          </section>
 
-          <div>
+          <section className="countries">
             <h3>.Chile</h3>
             <p>Roberto del Río 1137, Providencia.</p>
-          </div>
+          </section>
         </section>
       </section>
     </footer>
