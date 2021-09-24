e36f2684eb9ec7738eb9a87c6f2ae107f4cd9f14
Everton Otavio Afonso <evertonafonso0@gmail.com>
2021-09-24 14:09:47 -0300

    finalização da estilização do footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -79,6 +79,32 @@ function Cards() {
             </ul>
           </nav>
         </section>
+
+        <section className="footer-countries">
+          <article>
+            <h3>.Brasil</h3>
+            <p>Avenida Andrômeda, 2000. Bloco 6 e 8 Alphaville SP</p>
+            <p>R. Ifigenia Maria de Oliveira, 3793 Jr. Piratininga Franca SP</p>
+          </article>
+
+          <article>
+            <h3>.Argentina</h3>
+            <p>Soler 5518, 3 Piso C1425BYF Palermo Hollywood CABA</p>
+          </article>
+
+          <article>
+            <h3>.México</h3>
+            <p>
+              Blvd. Miguel de Cervantes Saavedra 169, Granada, Miguel Hidalgo,
+              11520 Ciudad de México, CDMX
+            </p>
+          </article>
+
+          <article>
+            <h3>.Chile</h3>
+            <p>Roberto del Río 1137, Providencia.</p>
+          </article>
+        </section>
       </section>
     </footer>
   );
