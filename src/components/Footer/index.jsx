399f4983c34744c62659eca20cec461a7cb22ac2
Everton Otavio Afonso <evertonafonso0@gmail.com>
2021-09-24 14:26:23 -0300

    testes para melhoramente de performance

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -81,29 +81,29 @@ function Cards() {
         </section>
 
         <section className="footer-countries">
-          <article>
+          <div>
             <h3>.Brasil</h3>
             <p>Avenida Andrômeda, 2000. Bloco 6 e 8 Alphaville SP</p>
             <p>R. Ifigenia Maria de Oliveira, 3793 Jr. Piratininga Franca SP</p>
-          </article>
+          </div>
 
-          <article>
+          <div>
             <h3>.Argentina</h3>
             <p>Soler 5518, 3 Piso C1425BYF Palermo Hollywood CABA</p>
-          </article>
+          </div>
 
-          <article>
+          <div>
             <h3>.México</h3>
             <p>
               Blvd. Miguel de Cervantes Saavedra 169, Granada, Miguel Hidalgo,
               11520 Ciudad de México, CDMX
             </p>
-          </article>
+          </div>
 
-          <article>
+          <div>
             <h3>.Chile</h3>
             <p>Roberto del Río 1137, Providencia.</p>
-          </article>
+          </div>
         </section>
       </section>
     </footer>
