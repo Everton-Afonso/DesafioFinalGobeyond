4491186118e8966ab080c2dcb9b9fda606832213
Everton Otavio Afonso <evertonafonso0@gmail.com>
2021-09-25 14:47:58 -0300

    usando memo

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,18 +1,15 @@
-import React, { useEffect, useState } from "react";
-
-// import api from "../../services/api";
+import React, { useEffect, useState, memo } from "react";
 
 import "./styles.css";
 
 function Main() {
-  // const [album, setAlbum] = useState([]);
-  const [album1, setAlbum1] = useState([]);
+  const [album, setAlbum] = useState([]);
   const [post, setPost] = useState(0);
 
   useEffect(() => {
     fetch("https://my-json-server.typicode.com/Everton-Afonso/db/album")
       .then((res) => res.json())
-      .then((res) => setAlbum1(res))
+      .then((res) => setAlbum(res))
       .catch((error) => {
         alert(`Ops! ocorreu um erro: ${error}`);
       });
@@ -22,7 +19,7 @@ function Main() {
     <main className="main-conteiner">
       <section className="main-content">
         <section className="main-text">
-          <h1>{album1[post]?.title}</h1>
+          <h1>{album[post]?.title}</h1>
           <a
             href="https://www.corebiz.ag/pt/"
             target="_blank"
@@ -36,10 +33,10 @@ function Main() {
 
         <section className="model-mini">
           <div>
-            {album1.map((itens, index) => (
+            {album.map((itens, index) => (
               <img
                 key={index}
-                onClick={() => setPost(itens.id)}
+                onClick={() => setPost(index)}
                 src={itens.thumbnailUrl}
                 alt="imagens do mini model"
               />
@@ -49,7 +46,7 @@ function Main() {
 
         <section className="model">
           <div>
-            <img src={album1[post]?.url} alt="imagens do model" />
+            <img src={album[post]?.url} alt="imagens do model" />
           </div>
         </section>
       </section>
@@ -57,4 +54,4 @@ function Main() {
   );
 }
 
-export default Main;
+export default memo(Main);
--- src/services/api.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "https://my-json-server.typicode.com/Everton-Afonso/db"
-});
-
-export default api;
