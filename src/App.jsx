02ea12010cf51c633cd58b608ef94a1a662c5e39
Everton Otavio Afonso <evertonafonso0@gmail.com>
2021-09-25 11:00:22 -0300

    teste

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Header from "./components/Header";
-import Main from "./components/Main";
+// import Main from "./components/Main";
 import Footer from "./components/Footer";
 
 import "./global.css";
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <Header />
-      <Main />
+      {/* <Main /> */}
       <Footer />
     </>
   );
