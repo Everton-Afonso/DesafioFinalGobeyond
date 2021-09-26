1c8e1ca62f1de8dae02447e0de7532ca9abeb004
Everton Otavio Afonso <evertonafonso0@gmail.com>
2021-09-25 23:40:08 -0300

    testando a performance dos 3 componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-// import Header from "./components/Header";
-// import Main from "./components/Main";
+import Header from "./components/Header";
+import Main from "./components/Main";
 import Footer from "./components/Footer";
 
 import "./global.css";
@@ -9,8 +9,8 @@ import "./global.css";
 function App() {
   return (
     <>
-      {/* <Header />
-      <Main /> */}
+      <Header />
+      <Main />
       <Footer />
     </>
   );
