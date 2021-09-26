8c76ca3e9ce0c1386e5a1084cd5b7f0c7ed47d96
Everton Otavio Afonso <evertonafonso0@gmail.com>
2021-09-25 23:25:28 -0300

    testando a header

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 
-
-import Main from "./components/Main";
-
+import Header from "./components/Header";
+// import Main from "./components/Main";
+// import Footer from "./components/Footer";
 
 import "./global.css";
 
 function App() {
   return (
     <>
-
-      <Main />
+      <Header />
+      {/* <Main /> */}
 
     </>
   );
